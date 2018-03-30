bd430be4ca83a56a8c7ad4ddfd9be2deb39dba75
Dennis Durairaj <dennis.durairaj@gmail.com>
2018-03-30 15:21:44 +0200

    Data driven components

--- app.js
+++ app.js
@@ -1,28 +1,66 @@
 class ProductList extends React.Component {
+  handleProductUpVote(productId) {
+    console.log(productId + ' was upvoted.');
+  }
   render() {
+    const products = Data.sort((a, b) => (
+      b.votes - a.votes
+    ));
+    const productComponents = Data.map((product) => (
+      <Product
+        key={'product-' + product.id}
+        id={product.id}
+        title={product.title}
+        description={product.description}
+        url={product.url}
+        votes={product.votes}
+        submitterAvatarUrl={product.submitter_avatar_url}
+        productImageUrl={product.product_image_url}
+        onVote={this.handleProductUpVote}
+      />
+    ))
     return (
       <div className="ui unstackable items">
-        <Product />
+        {productComponents}
       </div>
     );
   }
 }
 
 class Product extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.handleUpVote = this.handleUpVote.bind(this);
+  }
+  handleUpVote() {
+    this.props.onVote(this.props.id);
+  }
+
   render() {
     return (
       <div className='item'>
         <div className="image">
-          <img src="images/products/image-aqua.png" alt="" />
+          <img src={this.props.productImageUrl} alt="" />
         </div>
         <div className="middle aligned content">
+          <div className="header">
+            <a onClick={this.handleUpVote}>
+              <i className="large caret up icon"></i>
+            </a>
+            {this.props.votes}
+          </div>
           <div className="description">
-            <a>Fort Knight</a>
-            <p>Authentic renaissance actors, delivered in just two weeks.</p>
+            <a href={this.props.url}>
+              {this.props.title}
+            </a>
+            <p>
+              {this.props.description}
+            </p>
           </div>
           <div className="extra">
             <span>Submitted by:</span>
-            <img src="images/avatars/daniel.jpg" alt="" className="ui avatar image" />
+            <img src={this.props.submitterAvatarUrl} alt="" className="ui avatar image" />
           </div>
         </div>
       </div>
